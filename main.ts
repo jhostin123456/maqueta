a32a3677b4e9241169ce2315f61f7dff4b133892
jhostin123456 <144047641+jhostin123456@users.noreply.github.com>
2023-09-04 10:57:12 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    if (input.temperature() >= 30) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    if (input.temperature() <= 28) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+})
